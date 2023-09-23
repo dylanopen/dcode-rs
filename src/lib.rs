3fe96a886405435d738d4fb46e0a512724dfb513
Dylan Rogers <opendylan@proton.me>
2023-09-23 18:38:24 +0100

    0.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 pub mod console
 {
-	pub fn out<Text: Into<String>>(text: Text)
+	pub fn out<Text: Into<String>> (text: Text)
 	{
 		print!("{}", text.into());
+	}
+
+	pub fn outln<Text: Into<String>> (text: Text)
+	{
+		print!("{}\n", text.into());
 	}
 }
 
